450fbbfaddac8556578d617f0b5047e015ebe725
yasyfiana <13518143@std.stei.itb.ac.id>
2020-08-31 20:36:08 +0700

    Done

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var imgs = document.getElementById('img');
-var images = ['Image/afii.png','Image/afi2.png','Image/afi3.png'];
+var images = ['Image/afii.png','Image/afi2.png', 'Image/afi1.png', 'Image/afi3.png'];
 var x = 0;
 
 setInterval(slider, 2000);
@@ -13,28 +13,3 @@ function slider() {
   }
   imgs.innerHTML = "<img src=" + images[x - 1] + ">";
 }
-
-
-// const nextImageDelay = 3000;
-// let currentImageCounter = 0;
-
-// slideShowImg[currentImageCounter].style.display = "block";
-// setInterval(nextImage, nextImageDelay);
-
-// function nextImage(){
-//     slideShowImg[currentImageCounter].display = "none"
-//     currentImageCounter = currentImageCounter + 1;
-//     slideShowImg[currentImageCounter].display = "block"
-// }
-
-// var start=1;
-// function set_time()
-// {
-//     setInterval(image_show,2000);
-// }
-
-// function image_show()
-// {
-//     var image_data;
-
-// }
